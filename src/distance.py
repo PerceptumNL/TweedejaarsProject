d802a0bd1a712a97a47886acc90d82bfe7417535
Jorn Peters <jornpeters@gmail.com>
2014-06-09 14:16:35 +0200

    implemented cosine distance

--- src/distance.py
+++ src/distance.py
@@ -2,17 +2,19 @@
 
 import numpy as np
 
+def __dotproduct(a, b):
+    return sum(a.dot(b.T).data)
+
+def __norm(a):
+    return np.sqrt(__dotproduct(a,a))
+
 def euclidean(a, b):
     return np.sqrt(sum((a - b).data ** 2))
 
 def cosine(a, b):
-
-    
-    vec_a = a.toarray()
-    vec_b = b.toarray()
-    norm_a = np.linalg.norm(vec_a)
-    norm_b = np.linalg.norm(vec_b)
-    dot = vec_a.dot(vec_b.T)[0][0]
+    norm_a = __norm(a)
+    norm_b = __norm(b)
+    dot = __dotproduct(a,b)
 
     if norm_a * norm_b * dot == 0:
         return 1e99
