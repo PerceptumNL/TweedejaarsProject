4d90f1e7ccbd97a52b6d5ed5117460d443c85dce
Jorn Peters <jornpeters@gmail.com>
2014-06-19 10:31:25 +0200

    added distance measures

--- src/distance.py
+++ src/distance.py
@@ -23,6 +23,15 @@ def __norm(a):
     """
     return np.sqrt(__dotproduct(a,a))
 
+def __sum(a):
+    """
+    Returns the sum of all elements in the vector
+    """
+    if issparse(a):
+        return np.sum(a.data)
+    else:
+        return np.sum(a)
+
 def euclidean(a, b):
     """
     Returns the euclidean distance between vector a and vector b.
@@ -39,7 +48,67 @@ def cosine(a, b):
     dot = __dotproduct(a,b)
     
     # prevent ZeroDivisionErrors
-    if norm_a * norm_b * dot == 0:
-        return 0
+    if norm_a * norm_b == 0:
+        return 2
     else:
-        return -np.abs(dot / (norm_a*norm_b))
+        return np.abs((dot / float((norm_a*norm_b)))-1)
+
+def chi_square(a, b):
+    if issparse(a) or issparse(b):
+        a = a.toarray()
+        b = b.toarray()
+    a_total = np.sum(a)
+    b_total = np.sum(b)
+    a_norm = a / a_total
+    b_norm = b / b_total
+
+    
+    return sum([((a_norm[i] - b_norm[i])**2) / a_norm[i] for i in range(0,len(a_norm))])
+
+def correlation(a,b):
+    norm_a = __norm(a)
+    norm_b = __norm(b)
+    if norm_a * norm_b == 0:
+        return 0
+    a_norm = a / __norm(a)
+    b_norm = b / __norm(b)
+    return -__dotproduct(a,b)
+
+def intersection(a,b):
+    if issparse(a):
+        a = a.toarray()
+    if issparse(b):
+        b = b.toarray()
+    sum_a = np.sum(a)
+    sum_b = np.sum(b)
+
+    if (sum_a * sum_b) == 0:
+        return 0
+
+    a_norm = a / sum_a
+    b_norm = b / sum_b
+
+    return -sum([np.min([a_norm[i], b_norm[i]]) for i in range(0, len(a))])
+
+def bhattacharyya(a, b):
+    if issparse(a):
+        a = a.toarray()
+    if issparse(b):
+        b = b.toarray()
+
+    N = len(a)
+    a_bar = np.sum(a) / float(N)
+    b_bar = np.sum(b) / float(N)
+
+    if a_bar * b_bar == 0:
+        return 1
+
+    return np.sqrt(1- (1/np.sqrt(a_bar*b_bar*N**2))*np.sum(np.sqrt(a*b)))
+
+    
+
+    
+
+
+
+
